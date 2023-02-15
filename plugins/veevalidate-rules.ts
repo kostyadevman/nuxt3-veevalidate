54a9c38ed247bd85c014a30f464b18df5337d656
Konstantin <timohakt@yandex.ru>
2023-02-16 01:49:55 +0300

    add plural form to custom message

--- plugins/veevalidate-rules.ts
+++ plugins/veevalidate-rules.ts
@@ -1,7 +1,16 @@
 import { defineRule } from "vee-validate";
 import * as rules from "@vee-validate/rules";
 
+
+
 export default defineNuxtPlugin((nuxtApp) => {
+
+  // TODO: replace to helpers
+  function makePpural(number, titles) {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return `${titles[number % 100 > 4 && number % 100 < 20 ? 2 : cases[number % 10 < 5 ? number % 10 : 5]]}`;
+  }
+
   Object.keys(rules)
     .filter((k) => k !== "default")
     .forEach((rule) => {
@@ -15,11 +24,8 @@ export default defineNuxtPlugin((nuxtApp) => {
       }
 
       const indexOfBannedSymbol = value.indexOf(bannedSymbol);
-
-      if (value && indexOfBannedSymbol > 0) {
-        console.log(value)
-        console.log(indexOfBannedSymbol)
-        return `Вы ввели ${value.length} символов из ${limit} и есть запрещенный символ "*"`;
+        if (value.length && indexOfBannedSymbol !== -1) {
+        return `Вы ввели ${value.length} ${makePpural(value.length, ["символ", "символа", "символов"])} из ${limit} и есть запрещенный символ "*"`;
       }
       return true;
     })
